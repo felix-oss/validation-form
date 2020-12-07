782c85739e807e9ea6f9b4b76c9f1dc4c6fa576b
felix-oss <uv.mhjl@gmail.com>
2020-12-07 20:44:34 +0000

    update

--- src/main.js
+++ src/main.js
@@ -8,7 +8,37 @@ import "./assets/img/4geeks.ico";
 window.Send = function Send() {
   var x = document.forms["myForm"]["fname"].value;
   if (x == "") {
-    alert("Name must be filled out");
+    alert("All fields must be filled out");
+    return false;
+  }
+  var a = document.forms["myForm"]["aname"].value;
+  if (a == "") {
+    alert("All fields must be filled out");
+    return false;
+  }
+  var b = document.forms["myForm"]["bname"].value;
+  if (b == "") {
+    alert("All fields must be filled out");
+    return false;
+  }
+  var c = document.forms["myForm"]["cname"].value;
+  if (c == "") {
+    alert("All fields must be filled out");
+    return false;
+  }
+  var d = document.forms["myForm"]["dname"].value;
+  if (d == "") {
+    alert("All fields must be filled out");
+    return false;
+  }
+  var e = document.forms["myForm"]["ename"].value;
+  if (e == "") {
+    alert("All fields must be filled out");
+    return false;
+  }
+  var v = document.forms["myForm"]["vname"].value;
+  if (v == "") {
+    alert("All fields must be filled out");
     return false;
   }
 };
