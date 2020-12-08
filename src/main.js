1415ebc2a7445d777b4f4371e79f058e9354f23a
felix-oss <uv.mhjl@gmail.com>
2020-12-08 15:31:23 +0000

    update

--- src/main.js
+++ src/main.js
@@ -5,70 +5,81 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+var errorMsg = string => {
+  var alrt = document.querySelector("#alert");
+  alrt.classList.contains("d-none") && alrt.classList.toggle("d-none");
+  alrt.innerHTML += `<p> ${string} </p>`;
+};
+
 window.Send = function Send() {
+  var alrt = document.querySelector("#alert");
+  !alrt.classList.contains("d-none") && alrt.classList.toggle("d-none");
+  document
+    .querySelectorAll("input")
+    .forEach(inp => (inp.style.borderColor = "#ced4da"));
+  document
+    .querySelectorAll("#inputState")
+    .forEach(inp => (inp.style.borderColor = "#ced4da"));
   var x = document.forms["myForm"]["fname"].value;
   if (x == "") {
-    document.querySelector("#inputEmail4").style.background =
+    document.querySelector("#inputEmail4").style.borderColor =
       "rgb(223, 104, 104)";
+    errorMsg("Please Pick An Email");
   }
   var a = document.forms["myForm"]["aname"].value;
   if (a == "") {
-    document.querySelector("#inputPassword4").style.background =
+    document.querySelector("#inputPassword4").style.borderColor =
       "rgb(223, 104, 104)";
+    errorMsg("please pick a state");
   }
   var b = document.forms["myForm"]["bname"].value;
   if (b == "") {
-    document.querySelector("#validationDefaultUsername").style.background =
+    document.querySelector("#validationDefaultUsername").style.borderColor =
       "rgb(223, 104, 104)";
+    errorMsg("please pick a state");
   }
   var c = document.forms["myForm"]["cname"].value;
   if (c == "") {
-    document.querySelector("#firstn").style.background = "rgb(223, 104, 104)";
+    document.querySelector("#firstn").style.borderColor = "rgb(223, 104, 104)";
+    errorMsg("please pick a state");
   }
   var d = document.forms["myForm"]["dname"].value;
   if (d == "") {
-    document.querySelector("#secondn").style.background = "rgb(223, 104, 104)";
+    document.querySelector("#secondn").style.borderColor = "rgb(223, 104, 104)";
+    errorMsg("please pick a state");
   }
   var e = document.forms["myForm"]["ename"].value;
   if (e == "") {
-    document.querySelector("#inputCity").style.background =
+    document.querySelector("#inputCity").style.borderColor =
       "rgb(223, 104, 104)";
+    errorMsg("please pick a state");
   }
   var v = document.forms["myForm"]["vname"].value;
   if (v == "") {
-    document.querySelector("#inputZip").style.background = "rgb(223, 104, 104)";
+    document.querySelector("#inputZip").style.borderColor =
+      "rgb(223, 104, 104)";
+    errorMsg("please pick a state");
   }
   var e = document.getElementById("inputState");
   var strUser = e.options[e.selectedIndex].value;
 
   var strUser1 = e.options[e.selectedIndex].text;
   if (strUser == 0) {
-    alert("Please select a State");
+    document.querySelector("#inputState").style.borderColor =
+      "rgb(223, 104, 104)";
+    errorMsg("please pick a state");
   }
-  //   if ((document.getElementById("mastercard").checked = false)) {
-  //     alert("Please select a Payment Method");
-  //   }
-  //   if ((document.getElementById("visa").checked = false)) {
-  //     alert("Please select a Payment Method");
-  //   }
-  //   if ((document.getElementById("paypal").checked = false)) {
-  //     alert("Please select a Payment Method");
-  //   }
-  //   if ((document.getElementById("amexpress").checked = false)) {
-  //     alert("Please select a Payment Method");
+  //   var radios = document.getElementsByName("payment");
+  //   var formValid = false;
+
+  //   var i = 0;
+  //   while (!formValid && i < radios.length) {
+  //     if (radios[i].checked) formValid = true;
+  //     i++;
   //   }
 
-  var radios = document.getElementsByName("payment");
-  var formValid = false;
-
-  var i = 0;
-  while (!formValid && i < radios.length) {
-    if (radios[i].checked) formValid = true;
-    i++;
-  }
-
-  if (!formValid) alert("Please pick a payment method");
-  return formValid;
+  //   if (!formValid) alert("Please pick a payment method");
+  //   return formValid;
 };
 // maybe make it check multiple times like with && then try to change the d-none yk
 // make it so the red goes away if its filled
